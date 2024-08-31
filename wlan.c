44dd36742b8070dc6826719e62d02d1ff2458cf1
cyberdefsec <maindef0000@gmail.com>
2024-08-31 22:46:53 +0300

    Update wifipass

--- wlan.c
+++ wlan.c
@@ -63,12 +63,12 @@ void clear_wifi_info(wifi_info **wifi){
 
 
 void print(const wifi_info *wifi){
-    printf("==========================================================\n");
-    printf("#  %-15s %-15s %-10s %10s #\n", columns_name[0], columns_name[1], columns_name[2], columns_name[3]);
-    printf("==========================================================\n");
+    printf("=========================================================\n");
+    printf("# %-15s %-15s %-10s %10s #\n", columns_name[0], columns_name[1], columns_name[2], columns_name[3]);
+    printf("=========================================================\n");
     if(wifi != NULL){
         while(wifi != NULL){
-            printf("   %-15s %-15s %-10s %10s\n", wifi->ssid, wifi->auth, wifi->enc, wifi->key);
+            printf("  %-15s %-15s %-10s %10s\n", wifi->ssid, wifi->auth, wifi->enc, wifi->key);
             wifi = wifi->next;
         }
     }
@@ -107,11 +107,13 @@ HANDLE wlan_init(void){
 	return NULL;
 }
 
+
 int wlan_list_interfaces(HANDLE h, WLAN_INTERFACE_INFO_LIST **ifaces){
 	if(WlanEnumInterfaces(h, NULL, &(*ifaces)) == ERROR_SUCCESS)
 		return ERROR_SUCCESS;
 	return EOF;
 }
+
 
 int wlan_list_profiles(HANDLE h, WLAN_INTERFACE_INFO_LIST *ifaces, WLAN_PROFILE_INFO_LIST **profiles){
 	DWORD index = 0;
@@ -124,6 +126,7 @@ int wlan_list_profiles(HANDLE h, WLAN_INTERFACE_INFO_LIST *ifaces, WLAN_PROFILE_
 	}
 	return EOF;
 }
+
 
 int wlan_info_profiles(HANDLE h, WLAN_INTERFACE_INFO_LIST *ifaces, WLAN_PROFILE_INFO_LIST *profiles, wifi_info **wifi){
 	DWORD iface = 0;
@@ -144,6 +147,7 @@ int wlan_info_profiles(HANDLE h, WLAN_INTERFACE_INFO_LIST *ifaces, WLAN_PROFILE_
 	}
 	return EOF;
 }
+
 
 void wlan_clear(HANDLE h, WLAN_INTERFACE_INFO_LIST *ifaces, WLAN_PROFILE_INFO_LIST *profiles){
     DWORD index = 0;
