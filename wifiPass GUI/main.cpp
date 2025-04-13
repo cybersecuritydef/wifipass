8aa7818d1674ccf5fc091dc665e40658cd01308d
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2025-04-13 09:03:12 +0300

    Update main.cpp

--- wifiPass GUI/main.cpp
+++ wifiPass GUI/main.cpp
@@ -8,34 +8,6 @@
 
 #include "main.h"
 
-enum wxbuildinfoformat {
-    short_f, long_f };
-
-wxString wxbuildinfo(wxbuildinfoformat format)
-{
-    wxString wxbuild(wxVERSION_STRING);
-
-    if (format == long_f )
-    {
-#if defined(__WXMSW__)
-        wxbuild << _T("-Windows");
-#elif defined(__WXMAC__)
-        wxbuild << _T("-Mac");
-#elif defined(__UNIX__)
-        wxbuild << _T("-Linux");
-#endif
-
-#if wxUSE_UNICODE
-        wxbuild << _T("-Unicode build");
-#else
-        wxbuild << _T("-ANSI build");
-#endif // wxUSE_UNICODE
-    }
-
-    return wxbuild;
-}
-
-
 BEGIN_EVENT_TABLE(MainFrame, wxFrame)
 
 END_EVENT_TABLE()
