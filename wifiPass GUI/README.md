c29e7b4a654cb30629fee67f181dd872971d72b6
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-06-03 09:46:09 +0300

    Delete wifiPass GUI directory

--- wifiPass GUI/README.md
+++
@@ -1,9 +0,0 @@
-Description
-===========
-
-	WiFi password viewer 
-
-Requests
-========
-	OS: Windows
-	
--- wifiPass GUI/application.cpp
+++
@@ -1,8 +0,0 @@
-#include "application.h"
-
-Application::Application(int &argc, char **argv) : QApplication(argc, argv){
-    setApplicationName(tr("WiFiPassView"));
-    setApplicationDisplayName(tr("WiFiPassView"));
-    setWindowIcon(QIcon(":/ico/title.ico"));    
-}
-
